1b78da2875b1599b13965bc243b438e4814d1a4c
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-11-21 09:48:33 -0300

    feat: funções

---
+++ 02-funcoes.js
@@ -0,0 +1,27 @@
+// 02-funcoes.js
+
+
+/* Formas tradicionais */
+
+// sintaxe anônima associada á variavel/constante
+
+// obs: cuidado com a ordem, já que nesta sintaxe a 
+// função PRIMEIRO precisa ser declarada para somente depois
+// ser chamada/executada
+const exemplo1 = function(){
+    console.log("Função anônima!");
+};
+
+exemplo1();
+
+/* Sintaxe declarada/nomeada 
+
+Obs.: nesta sintaxe, você pode chamar a função
+antes ou depois de declará-la. Portanto, a ordem
+não import
+*/
+
+exemplo2();
+function exemplo2(){
+    console.log("Função nomeada!");
+};
