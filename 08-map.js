ca966d883d8a8694a59e7e1acc59f4644b1d1c87
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-11-23 11:16:09 -0300

    Exercicio
    Use o map para gerar um novo array APENAS com os preços
    calculados com um desconto de 10%.

--- 08-map.js
+++ 08-map.js
@@ -1,8 +1,9 @@
 // 08-map.js
+import cursos from "./modulos/cursos.js";
 
 /* map (mapear, mapeia - transformar em outra coisa)
 Passa por elementos de um array e realiza operações 
-em cada um deles atraves de uma função gerando um novo */
+em cada um deles atraves de uma função (callback) gerando um novo */
 
 //Exemplo 1
 const numeros = [10, 20, 5, 12, 333, 50];
@@ -13,3 +14,32 @@ const numerosDobrados = numeros.map(
 for (const numerosDobrado of numerosDobrados ){
     console.log(numerosDobrado);
 }
+
+console.log("----------------");
+
+console.log(cursos);
+
+// Exemplo 2: Gerar um novo Array apenas com os nomes dos cursos
+const titulos = cursos.map( curso =>  curso.titulo);
+
+console.log(titulos);
+
+/* Exercicio  
+Use o map para gerar um novo array APENAS com os preços
+calculados com um desconto de 10%.
+
+Como calcular?
+-Opção 1: valor - valor * 0.10
+-Opção 2: valor * 0.9               
+
+E mostre no console.*/
+
+const blackFriday = cursos.map(
+    curso => {
+        let desconto = curso.preco * 0.10;
+        let precoFinal = Math.abs(desconto - curso.preco);
+        return ["Preço: R$"+curso.preco+",00", "Desconto: R$"+desconto+",00", "Preço com deconto: R$"+precoFinal+",00"]
+    }
+);
+
+console.log(blackFriday);
