70d544b6b545fea142e4cc8befaadfd29084ada5
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-11-24 09:01:05 -0300

    Feat:  filter (filtrar)

---
+++ 09-filter.js
@@ -0,0 +1,33 @@
+// 09-filter.js
+
+/* filter (filtrar) 
+Passa por elementos de um array (usando uma função callback) e retornar valores de acordo com uma ou mais condições gerando um novo array. */
+
+// Exemplo 01
+const vendas = [1500,3000,1000,500,750,5000,2000];
+const meta = 1000;
+
+// gerando um array com os valores que bateram a meta
+
+const vendasAcimaDaMeta = vendas.filter(venda =>   venda >= meta);
+
+console.log(vendasAcimaDaMeta);
+
+// Exemplo 02
+
+const alunos = [
+    "Nicolas", "Eliel", "André", 
+    "Kaue", "Barbosa", "Aline", 
+    "Melissa","Marina", "Mônica",
+    "Marcelo"
+];
+
+// Filtrando Aluno que começam com "M" e terminam com "o" 
+// obs: Faz diferença se usar maiúsculas ou minusculas
+
+const resultados = alunos.filter( aluno =>  aluno.startsWith("M") && aluno.endsWith("o")
+    // aluno.startsWith("M") || aluno.startsWith("m")
+
+);
+
+console.log(resultados);
